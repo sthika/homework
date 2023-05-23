af3bf5cf6659ac2dd5a021a8603ef6aec5a9ca52
sthika <sveta.tyo45@gmail.com>
2023-05-23 22:01:39 +0600

    change second task

--- object/script.js
+++ object/script.js
@@ -1,16 +1,16 @@
 // 1. Создать объект user с полями name, age и email. Вывести значения полей в консоль.
 
-const user = {
-    name: "Sveta",
-    age: 25,
-    email: "sveta@mail.ru",
-}
-console.log(Object.values(user)) //      Вывести значения полей в консоль.
-user.id = 1;                     //      -Добавить ключ id со значением 1 
-user["second name"] = "Svetlana" //      -Добавить ключ second name
-delete user.age                 //      -Удалить ключ age
-user.email = "sveta@gmail.com"  //      -Изменит значение ключа email
-Object.freeze(user)             //      -Сделать так чтоб в дальнейшем этот объект не могли изменять
+// const user = {
+//     name: "Sveta",
+//     age: 25,
+//     email: "sveta@mail.ru",
+// }
+// console.log(Object.values(user)) //      Вывести значения полей в консоль.
+// user.id = 1;                     //      -Добавить ключ id со значением 1 
+// user["second name"] = "Svetlana" //      -Добавить ключ second name
+// delete user.age                 //      -Удалить ключ age
+// user.email = "sveta@gmail.com"  //      -Изменит значение ключа email
+// Object.freeze(user)             //      -Сделать так чтоб в дальнейшем этот объект не могли изменять
 
 
 // 2. Создайте объект заработных плат obj = {Ширин: '1000',
@@ -20,27 +20,31 @@ const obj = {
     Айжан:'500', 
     Атай: '200'
 }
-                            //                решение 1
+                                        //    решение 1
+
+let newArr = []
 Object.keys(obj).forEach((key) => {
     if (key === 'Айжан' || key === 'Атай') {
-        console.log(`zp ${key}: ${obj[key]}`);
+        newArr = [...newArr, `${obj[key]}`];
     }
 })
-                           //                решение 2
-console.log(obj["Айжан"], obj["Атай"])  // через [] так как ключи написаны на русском
+console.log(newArr)
+
+                                        //   решение 2
+console.log([obj["Айжан"], obj["Атай"]])  // через [] так как ключи написаны на русском
 
 
 // 3. Создать объект team с полем members, представляющим собой массив имен участников. 
 //    Добавить нового участника в массив и вывести обновленный объект в консоль. 
 //    (Учитываем, то что мы можем не знать список участников которые уже записаны)
 
-const team = {
-    members: []
-}
+// const team = {
+//     members: []
+// }
 
-let addMember = (obj, name) => {
-    obj.members = [...obj.members, name] 
-    return obj;
-}
-addMember(team, "Sveta")
-console.log(team)
+// let addMember = (obj, name) => {
+//     obj.members = [...obj.members, name] 
+//     return obj;
+// }
+// addMember(team, "Sveta")
+// console.log(team)
